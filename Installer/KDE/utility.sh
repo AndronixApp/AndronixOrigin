f016f9c845473afa1fe2e606b5684634d7237162
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2019-09-02 17:51:38 +0530

    Create utility.sh
    KDE Utility installer😜⭐📑

---
+++ Installer/KDE/utility.sh
@@ -0,0 +1,17 @@
+clear
+echo " "
+echo " "
+echo "------------------------------------------------"
+echo " "
+echo "STARTING THE INSTALLATION ON ADDITIONAL PACKAGES"
+echo " "
+echo "                                    -BY ANDRONIX"
+echo " "
+echo " "
+apt update
+apt install gwenviwe kcolorchooser gimp -y
+apt install xorg -y
+echo " "
+echo "-----------------------------------------------"
+echo "PROCESS COMPLETED"
+exit 1
