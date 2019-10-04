2287e0a74c2a2dd384a6ef6c882ea579f5bcf7d0
Aerox912 <nick@nickos.tech>
2019-10-04 12:44:26 +0200

    Do the same for the x64 rootfs
    Since most probably it follows the same rules :)

--- Installer/Arch/amd64/additional.sh
+++ Installer/Arch/amd64/additional.sh
@@ -10,14 +10,19 @@ echo ""
 echo "Changing some permissions, please be patient"
 echo ""
 echo ""
-chmod 755 -R /bin /home /mnt /run /srv /tmp /var /boot /etc /lin /opt /root /sbin /sys /usr
+chmod 755 -R /bin /home /mnt /run /srv /tmp /var /boot /etc /opt /root /sbin /sys /usr
 echo "IMPORTANT"
 echo ""
 echo ""
+echo "Removing some unused packages, reclaims about 650MB"
+yes | LC_ALL=en_US.UTF-8 pacman -Rncs linux-firmware systemd-sysvinit openresolv mdadm lvm2 reiserfsprogs usbutils
 echo "If you are using Android 9 and above, you will encounter this error:"
 echo ""
 echo "could not change the root directory (Function not implemented)"
 echo ""
 echo "Simply ignore it as it does not do anything harmful"
+echo ""
+echo "updating Arch packages"
+yes | LC_ALL=en_US.UTF-8 pacman -Suuyy 
 echo ""
 echo ""
