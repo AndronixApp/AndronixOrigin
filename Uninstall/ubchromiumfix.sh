a56a09a6370a167fb5ae64901d0451549dc3eb52
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2019-10-15 23:07:33 +0530

    Update ubchromiumfix.sh

--- Uninstall/ubchromiumfix.sh
+++ Uninstall/ubchromiumfix.sh
@@ -6,3 +6,4 @@ apt install software-properties-common
 echo "deb http://ppa.launchpad.net/chromium-team/stable/ubuntu bionic main 
 deb-src http://ppa.launchpad.net/chromium-team/stable/ubuntu bionic main" >> /etc/apt/sources.list
 apt update && apt install chromium-browser
+apt install chromium-browser
