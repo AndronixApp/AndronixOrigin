0612b468b0a6e85f785b7d8c17744b20842c510c
Aerox912 <nick@nickos.tech>
2019-10-19 01:33:42 +0200

    Several improvements for chromium fix
    - Moved the apt update to software-properties line and silenced it.
    - Purge both chromium/chromium browser in the same command and also silenced it. ( -qq = assume yes and show minimal output)
    - Added echo's to replace the scripted output.
    - Added import of GPG keys since Debian does not allow updating from the launchpad without the key. Probably also applies to Kali/Parrot.
    - Patch chromium-browser.desktop file.
    - Add bash alias to easily access the browser.

--- Uninstall/ubchromiumfix.sh
+++ Uninstall/ubchromiumfix.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-apt update
-apt purge chromium-broswer* -y
-apt purge chromium* -y
-apt install software-properties-common
+echo "Removing distribution provided chromium packages and dependencies..."
+apt purge chromium* chromium-broswer* -qq && apt autoremove -qq
+ehco "Enabling PPA support..."
+apt update -qq && apt install software-properties-common --no-install recommends -qq
+echo " Adding chromium-team stable ppa"
 echo "deb http://ppa.launchpad.net/chromium-team/stable/ubuntu bionic main 
 deb-src http://ppa.launchpad.net/chromium-team/stable/ubuntu bionic main" >> /etc/apt/sources.list
-apt update && apt install chromium-browser
-apt install chromium-browser
+echo "Fetching and importing chromium-team GPG keys..."
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0EBEA9B02842F111
+echo "Installing chromium-browser"
+apt update && apt install chromium-browser -qq
+echo "Patching application shortcuts..."
+sed -i 's/chromium-browser/chromium-broswer --no-sandbox/g' /usr/share/applications/chromium-browser.desktop
+echo 'alias chromium="chromium-browser --no-sandbox" >> /etc/profile'
+echo "You can now start chromium by using the application icon or by typing chromium" && . /etc/profile
