bfa5dc9d1791cfd04a1e3cfed51d7c633f181923
Aerox912 <nick@nickos.tech>
2019-10-21 13:32:51 +0200

    Small fix for chromium patch.
    Works without errors on kde ubuntu now also. And does not result in duplicate --no-sandbox in the .desktop file.

--- Uninstall/ubchromiumfix.sh
+++ Uninstall/ubchromiumfix.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo "Removing distribution provided chromium packages and dependencies..."
-apt purge chromium* chromium-broswer* -qq && apt autoremove -qq
+apt purge chromium* chromium-broswer* -y -qq && apt autoremove -y -qq
 echo "Enabling PPA support..."
-apt update -qq && apt install software-properties-common --no-install-recommends -qq
+apt update -qq && apt install software-properties-common gnupg --no-install-recommends -y -qq
 echo " Adding chromium-team stable ppa"
 echo "deb http://ppa.launchpad.net/chromium-team/stable/ubuntu bionic main 
 deb-src http://ppa.launchpad.net/chromium-team/stable/ubuntu bionic main" >> /etc/apt/sources.list
 echo "Fetching and importing chromium-team GPG keys..."
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0EBEA9B02842F111
 echo "Installing chromium-browser"
-apt update && apt install chromium-browser -qq
+apt update && apt install chromium-browser --no-install-recommends -y -qq
 echo "Patching application shortcuts..."
-sed -i 's/chromium-browser/chromium-broswer --no-sandbox/g' /usr/share/applications/chromium-browser.desktop
+sed -i 's/chromium-browser %U/chromium-broswer --no-sandbox %U/g' /usr/share/applications/chromium-browser.desktop
 echo 'alias chromium="chromium-browser --no-sandbox" >> /etc/profile'
 echo "You can now start chromium by using the application icon or by typing chromium" && . /etc/profile
