6f6f17f636f622c57956b6926123775face3790d
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2019-11-17 16:45:37 +0530

    Create ubuntu19.sh

---
+++ Rootfs/Ubuntu19/ubuntu19.sh
@@ -0,0 +1,103 @@
+#!/data/data/com.termux/files/usr/bin/bash
+folder=ubuntu19-fs
+if [ -d "$folder" ]; then
+	first=1
+	echo "skipping downloading"
+fi
+tarball="ubuntu19-rootfs.tar.xz"
+
+if [ "$first" != 1 ];then
+	if [ ! -f $tarball ]; then
+		echo "Download Rootfs, this may take a while base on your internet speed."
+		case `dpkg --print-architecture` in
+		aarch64)
+			archurl="arm64" ;;
+		arm)
+			archurl="armhf" ;;
+		amd64)
+			archurl="amd64" ;;
+		x86_64)
+			archurl="amd64" ;;	
+		i*86)
+			archurl="i386" ;;
+		x86)
+			archurl="i386" ;;
+		*)
+			echo "unknown architecture"; exit 1 ;;
+		esac
+		wget "https://andronixos.sfo2.digitaloceanspaces.com/Ubuntu19/ubuntu19-rootfs-${archurl}.tar.xz" -O $tarball
+
+fi
+	cur=`pwd`
+	mkdir -p "$folder"
+	cd "$folder"
+	echo "Decompressing Rootfs, please be patient."
+	proot --link2symlink tar -xJf ${cur}/${tarball}||:
+	cd "$cur"
+fi
+mkdir -p ubuntu19-binds
+bin=start-ubuntu19.sh
+echo "writing launch script"
+cat > $bin <<- EOM
+#!/bin/bash
+cd \$(dirname \$0)
+## unset LD_PRELOAD in case termux-exec is installed
+unset LD_PRELOAD
+command="proot"
+command+=" --link2symlink"
+command+=" -0"
+command+=" -r $folder"
+if [ -n "\$(ls -A ubuntu19-binds)" ]; then
+    for f in ubuntu19-binds/* ;do
+      . \$f
+    done
+fi
+command+=" -b /dev"
+command+=" -b /proc"
+command+=" -b ubuntu19-fs/root:/dev/shm"
+## uncomment the following line to have access to the home directory of termux
+#command+=" -b /data/data/com.termux/files/home:/root"
+## uncomment the following line to mount /sdcard directly to / 
+#command+=" -b /sdcard"
+command+=" -w /root"
+command+=" /usr/bin/env -i"
+command+=" HOME=/root"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+command+=" TERM=\$TERM"
+command+=" LANG=C.UTF-8"
+command+=" /bin/bash --login"
+com="\$@"
+if [ -z "\$1" ];then
+    exec \$command
+else
+    \$command -c "\$com"
+fi
+EOM
+
+mkdir ~/ubuntu19-fs/usr/share/andronix
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/.bash_profile -P ~/ubuntu19-fs/root/
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/.profile -P ~/ubuntu19-fs/etc/skel
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/.profile -P ~/ubuntu19-fs/root/
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/firstrun -P ~/ubuntu19-fs/usr/share/andronix
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vnc -P ~/ubuntu19-fs/usr/local/bin
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vncpasswd -P ~/ubuntu19-fs/usr/local/bin
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vncserver-stop -P ~/ubuntu19-fs/usr/local/bin
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vncserver-start -P ~/ubuntu19-fs/usr/local/bin
+
+chmod +x ~/ubuntu19-fs/root/.bash_profile
+chmod +x ~/ubuntu19-fs/etc/skel/.profile
+chmod +x ~/ubuntu19-fs/root/.profile
+chmod +x ~/ubuntu19-fs/usr/share/andronix/firstrun
+chmod +x ~/ubuntu19-fs/usr/local/bin/vnc
+chmod +x ~/ubuntu19-fs/usr/local/bin/vncpasswd
+chmod +x ~/ubuntu19-fs/usr/local/bin/vncserver-start
+chmod +x ~/ubuntu19-fs/usr/local/bin/vncserver-stop
+
+
+echo "fixing shebang of $bin"
+termux-fix-shebang $bin
+echo "making $bin executable"
+chmod +x $bin
+echo "removing image for some space"
+rm $tarball
+echo "You can now launch Ubuntu with the ./${bin} script"
