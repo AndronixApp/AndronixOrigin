02fd7a32e923a0d7cf7c4a1b4131a267ac83ca01
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2019-11-19 20:35:42 +0530

    MATE for Ubuntu19

---
+++ APT/MATE/mate19.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#Get the necessary components
+sudo apt-mark hold udisks2
+sudo apt-get update
+sudo apt-get install mate-desktop-environment-core mate-terminal tightvncserver -y
+sudo apt-get install xfe -y
+sudo apt-get clean
+
+
+echo " "
+echo "You can now start vncserver by running vncserver-start"
+echo " "
+echo "It will ask you to enter a password when first time starting it."
+echo " "
+echo "The VNC Server will be started at 127.0.0.1:5901"
+echo " "
+echo "You can connect to this address with a VNC Viewer you prefer"
+echo " "
+echo "Connect to this address will open a window with Mate Desktop Environment"
+echo " "
+echo " "
+echo " "
+echo "Running vncserver-start"
+echo " "
+echo " "
+echo " "
+echo "To Kill VNC Server just run vncserver-stop"
+echo " "
+echo " "
+echo " "
+
+echo "export DISPLAY=":1"" >> /etc/profile
+source /etc/profile
+
+vncserver-start
