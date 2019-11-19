be1d6cd5fd159dcaae70df39a689f01a64cde2de
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2019-11-19 20:18:16 +0530

    LXDE Installer Ubuntu 19📜

---
+++ APT/LXDE/lxde19.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#Get the necessary components
+sudo apt-mark hold udisks2
+sudo apt-get update
+sudo apt-get install lxde-core lxterminal tightvncserver -y
+sudo apt-get install xfe -y
+sudo apt-get clean
+
+#Setup the necessary files
+mkdir ~/.vnc
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/LXDE/xstartup -P ~/.vnc/
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/LXDE/vncserver-start -P /usr/local/bin/
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/LXDE/vncserver-stop -P /usr/local/bin/
+
+chmod +x ~/.vnc/xstartup
+chmod +x /usr/local/bin/vncserver-start
+chmod +x /usr/local/bin/vncserver-stop
+
+echo " "
+echo "You can now start vncserver by running vncserver-start"
+echo " "
+echo "It will ask you to enter a password when first time starting it."
+echo " "
+echo "The VNC Server will be started at 127.0.0.1:5901"
+echo " "
+echo "You can connect to this address with a VNC Viewer you prefer"
+echo " "
+echo "Connect to this address will open a window with LXDE Desktop Environment"
+echo " "
+echo " "
+echo " "
+echo "Running vncserver-start"
+echo " "
+echo " "
+echo " "
+echo "To Kill VNC Server just run vncserver-stop"
+echo " "
+echo " "
+echo " "
+
+echo "export DISPLAY=":1"" >> /etc/profile
+source /etc/profile
+
+vncserver-start
