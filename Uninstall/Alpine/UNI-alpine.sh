10506eb6075add7dde72c0f6df922a7e70dbc760
Nick <39168440+Aerox912@users.noreply.github.com>
2019-11-22 23:03:53 +0100

    Create UNI-alpine.sh

---
+++ Uninstall/Alpine/UNI-alpine.sh
@@ -0,0 +1,11 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "Starting to uninstall, please be patient..."
+
+chmod 777 -R alpine-fs
+rm -rf alpine-fs
+rm -rf alpine-binds
+rm -rf alpine.sh
+rm -rf start-alpine.sh
+rm -rf UNI-alpine.sh
+echo "Done"
