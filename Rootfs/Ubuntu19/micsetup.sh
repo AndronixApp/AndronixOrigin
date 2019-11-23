20a1d2db7ad3a6fbecac4d2d04dab06f44b9e0c9
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2019-11-23 17:28:45 +0530

    Add files via upload

---
+++ Rootfs/Ubuntu19/micsetup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+clear
+echo "Setting up serives..."
+echo "Downloading required necessary packages"
+sudo apt install ffmpeg -y
+echo "Setting up config files..."
+echo "
+echo "Starting Audio input service..."
+echo ""
+echo "Make sure you have started the server from the app with following config"
+echo ""
+echo "---------------------------"
+echo "							|"
+echo " TARGET ADDRESS: 127.0.0.1|"
+echo " TARGET PORT: 55555		|" 
+echo " MODE: Manual				|"
+echo " FORMAT: G.726			|"
+echo "							|"
+echo "---------------------------"
+echo ""
+ffplay rtp://127.0.0.1:55555
+echo ""
+echo " Black window will popup if Audio Input has been enabled" 
+echo " To stop audio input close the window"
+echo "If no window pops up check you have started server with proper config"
+echo "Also open new Termux session and type pluseaudio --start"
+
+" >> /usr/local/bin/startmic 
+chmod +x /usr/local/bin/startmic 
+
+echo "To start the mic input type: startmic"
