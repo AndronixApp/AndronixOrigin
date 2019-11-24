c32c71b374a5a0c613e7a62a9892bdd6f44af174
Nick <39168440+Aerox912@users.noreply.github.com>
2019-11-24 17:05:08 +0100

    Xfce19 call custom vncpasswd script
    And silence mkdir

--- APT/XFCE4/xfce19.sh
+++ APT/XFCE4/xfce19.sh
@@ -5,7 +5,7 @@ sudo apt-mark hold udisks2
 sudo apt-get update
 sudo apt-get install xfce4 xfce4-terminal tigervnc-standalone-server tigervnc-common dbus-x11 --no-install-recommends -y
 sudo apt-get clean
-mkdir ~/.vnc
+mkdir -p ~/.vnc
 echo "#!/bin/bash
 [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
 export PULSE_SERVER=127.0.0.1
@@ -40,4 +40,5 @@ echo " "
 echo "export DISPLAY=":1"" >> /etc/profile
 source /etc/profile
 
+vncpasswd
 vncserver-start
