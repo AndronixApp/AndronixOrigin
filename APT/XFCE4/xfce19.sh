197f8633691fa7f1093a20a8cf7574ff6f760060
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2019-12-09 15:37:26 +0530

    Added chromium pre-installation

--- APT/XFCE4/xfce19.sh
+++ APT/XFCE4/xfce19.sh
@@ -21,6 +21,11 @@ echo $$ > /tmp/xsession.pid
 dbus-launch --exit-with-session startxfce4 &' > ~/.vnc/xstartup
 
 echo " "
+
+echo "Running browser patch"
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Uninstall/ubchromiumfix.sh && chmod +x ubchromiumfix.sh
+./ubchromiumfix.sh && rm -rf ubchromiumfix.sh
+
 echo "You can now start vncserver by running vncserver-start"
 echo " "
 echo "It will ask you to enter a password when first time starting it."
