e9aea3c20c96195e35aa1d0567dda3b9b8dbcdf2
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2019-12-09 15:36:06 +0530

    Added chromium pre-installation

--- APT/MATE/mate19.sh
+++ APT/MATE/mate19.sh
@@ -18,6 +18,11 @@ xrdb $HOME/.Xresources
 mate-session &" > ~/.vnc/xstartup
 
 echo " "
+
+echo "Running browser patch"
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Uninstall/ubchromiumfix.sh && chmod +x ubchromiumfix.sh
+./ubchromiumfix.sh && rm -rf ubchromiumfix.sh
+
 echo "You can now start vncserver by running vncserver-start"
 echo " "
 echo "It will ask you to enter a password when first time starting it."
