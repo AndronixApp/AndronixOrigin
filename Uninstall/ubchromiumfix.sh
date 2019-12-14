925ac1d2f2d3e48796ddc0001c92fa8dd8988b10
Nick <39168440+Aerox912@users.noreply.github.com>
2019-12-14 21:40:12 +0100

    Update ubchromiumfix.sh

--- Uninstall/ubchromiumfix.sh
+++ Uninstall/ubchromiumfix.sh
@@ -2,14 +2,14 @@
 echo "Removing distribution provided chromium packages and dependencies..."
 apt purge chromium* chromium-browser* -y -qq && apt autoremove -y -qq
 echo "Enabling PPA support..."
-apt update -qq && apt install software-properties-common gnupg --no-install-recommends -y -qq
+[ ! -f .parrot ] && apt update -qq; apt install software-properties-common gnupg --no-install-recommends -y -qq
 echo " Adding chromium-team stable ppa"
 echo "deb http://ppa.launchpad.net/chromium-team/stable/ubuntu bionic main 
 deb-src http://ppa.launchpad.net/chromium-team/stable/ubuntu bionic main" >> /etc/apt/sources.list
 echo "Fetching and importing chromium-team GPG keys..."
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0EBEA9B02842F111
 echo "Installing chromium-browser"
-apt update && apt install chromium-browser --no-install-recommends -y -qq
+apt update -qq && apt install chromium-browser --no-install-recommends -y -qq
 echo "Patching application shortcuts..."
 sed -i 's/chromium-browser %U/chromium-browser --no-sandbox %U/g' /usr/share/applications/chromium-browser.desktop
 echo 'alias chromium="chromium-browser --no-sandbox" >> /etc/profile'
