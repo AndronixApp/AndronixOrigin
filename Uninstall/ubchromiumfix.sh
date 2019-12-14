f029942d6d8d59d64431b6ed6f27a29973fb32ae
Nick <39168440+Aerox912@users.noreply.github.com>
2019-12-14 22:08:35 +0100

    Changes for parrot 🦜
    Small changes to allow chromium install to pass on 🦜 even though update partially fails

--- Uninstall/ubchromiumfix.sh
+++ Uninstall/ubchromiumfix.sh
@@ -9,7 +9,7 @@ deb-src http://ppa.launchpad.net/chromium-team/stable/ubuntu bionic main" >> /et
 echo "Fetching and importing chromium-team GPG keys..."
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0EBEA9B02842F111
 echo "Installing chromium-browser"
-apt update -qq && apt install chromium-browser --no-install-recommends -y -qq
+apt update -qq; apt install chromium-browser --no-install-recommends -y
 echo "Patching application shortcuts..."
 sed -i 's/chromium-browser %U/chromium-browser --no-sandbox %U/g' /usr/share/applications/chromium-browser.desktop
 echo 'alias chromium="chromium-browser --no-sandbox" >> /etc/profile'
