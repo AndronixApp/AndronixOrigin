acab0d1aea74dae3f8e169d7907b5c55b56357cc
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-01-07 22:10:25 +0530

    LibreOffice Patch Fix

--- Uninstall/librepatch.sh
+++ Uninstall/librepatch.sh
@@ -3,5 +3,5 @@ sudo apt install libreoffice --no-install-recommends -y
 rm -rf /usr/lib/libreoffice/program/oosplash 
 wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Uninstall/oosplash?raw=true  -q -O /usr/lib/libreoffice/program/oosplash
 chmod +x /usr/lib/libreoffice/program/oosplash
-
+mkdir /prod && mkdir /prod/version
 echo "Patch has been applied successfully"
