1e59df00fe24bef5a47259ffe1c854668acb36a6
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-03-23 17:32:45 +0530

    Create UNI-void.sh

---
+++ Uninstall/Void/UNI-void.sh
@@ -0,0 +1,12 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "Starting to uninstall, please be patient..."
+
+chmod 777 -R void-fs
+rm -rf void-fs
+rm -rf void-binds
+rm -rf void.sh
+rm -rf start-void.sh
+rm -rf UNI-ubuntu.sh
+
+echo "Done"
