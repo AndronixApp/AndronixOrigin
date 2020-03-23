a74ba907681229edd9babb2db6a61293a7e7575a
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-03-23 23:38:51 +0530

    Added font installation📃

--- XBPS/XFCE4/xfce4_de.sh
+++ XBPS/XFCE4/xfce4_de.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 xbps-install -Su
-xbps-install -S xfce4 tigervnc wget 
+xbps-install -S xfce4 tigervnc xorg wget 
 mkdir ~/.vnc
 
 wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/XBPS/XFCE4/vncserver-start -P /usr/local/bin/
