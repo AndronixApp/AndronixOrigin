1ac3289139ad902333a8223e5b7a9d288a312ef3
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-04-06 16:03:01 +0530

    Update manjaro.sh

--- Installer/Manjaro/manjaro.sh
+++ Installer/Manjaro/manjaro.sh
@@ -60,6 +60,7 @@ echo "making $bin executable"
 chmod +x $bin
 echo "fixing permissions for manjaro"
 chmod 755 -R manjaro-fs
-echo "pacman-mirrors -g && pacman -Syyuu --noconfirm" > $folder/root/.bash_profile
+rm -rf $folder/etc/resolv.conf && echo "namserver 1.1.1.1" > $folder/etc/resolv.conf
+echo "pacman-mirrors -g -c The_Netherlands && pacman -Syyuu --noconfirm" > $folder/root/.bash_profile
 echo "You can now launch Manjaro Linux with the ./${bin} script next time"
 bash $bin
