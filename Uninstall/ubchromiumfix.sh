10b5f7a9acf697b754a612fe643924f300602d11
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-04-18 10:56:42 +0530

    Update ubchromiumfix.sh

--- Uninstall/ubchromiumfix.sh
+++ Uninstall/ubchromiumfix.sh
@@ -4,10 +4,10 @@ apt purge chromium* chromium-browser* -y -qq && apt autoremove -y -qq
 echo "Enabling PPA support..."
 [ ! -f .parrot ] && apt update -qq; apt install software-properties-common gnupg --no-install-recommends -y -qq
 echo " Adding chromium-team stable ppa"
-echo "deb http://ppa.launchpad.net/chromium-team/stable/ubuntu bionic main 
-deb-src http://ppa.launchpad.net/chromium-team/stable/ubuntu bionic main" >> /etc/apt/sources.list
+echo "deb http://ppa.launchpad.net/canonical-chromium-builds/stage/ubuntu bionic main 
+deb-src http://ppa.launchpad.net/canonical-chromium-builds/stage/ubuntu bionic main " >> /etc/apt/sources.list
 echo "Fetching and importing chromium-team GPG keys..."
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0EBEA9B02842F111
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8FEA526CE21182D1
 echo "Installing chromium-browser"
 apt update -qq; apt install chromium-browser --no-install-recommends -y
 echo "Patching application shortcuts..."
