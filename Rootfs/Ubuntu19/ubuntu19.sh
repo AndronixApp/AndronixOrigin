c751edebd0c56b1cce89beb4c8d656237e65f9db
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-04-18 09:29:23 +0530

    Update ubuntu19.sh

--- Rootfs/Ubuntu19/ubuntu19.sh
+++ Rootfs/Ubuntu19/ubuntu19.sh
@@ -32,7 +32,7 @@ fi
 	mkdir -p "$folder"
 	cd "$folder"
 	echo "Decompressing Rootfs, please be patient."
-	proot --link2symlink tar -xJf ${cur}/${tarball} --exclude=dev||:
+	proot --link2symlink tar -xf ${cur}/${tarball} --exclude=dev||:
 	cd "$cur"
 fi
 mkdir -p ubuntu19-binds
