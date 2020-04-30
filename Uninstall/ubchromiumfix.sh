61afe95cbd6b4d61025ad669c2f6e12ca008df86
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-04-30 14:09:37 +0530

    Fix for debian based distro

--- Uninstall/ubchromiumfix.sh
+++ Uninstall/ubchromiumfix.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 echo "Removing distribution provided chromium packages and dependencies..."
+apt purge chromium* chromium-browser* -y -qq && apt autoremove -y -qq
 sudo apt purge chromium* chromium-browser* -y -qq && apt autoremove -y -qq
 echo "Enabling PPA support..."
 [ ! -f .parrot ] && apt update -qq; apt install software-properties-common gnupg --no-install-recommends -y -qq
 echo " Adding chromium-team stable ppa"
-sudo add-apt-repository ppa:ultrahacx/chromium-universal -y
+echo "deb http://ppa.launchpad.net/ultrahacx/chromium-universal/ubuntu bionic main 
+deb-src http://ppa.launchpad.net/ultrahacx/chromium-universal/ubuntu bionic main " >> /etc/apt/sources.list
+echo "Fetching and importing chromium-team GPG keys..."
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8FEA526CE21182D1
+echo "Installing chromium-browser"
 apt update -qq; apt install chromium-browser --no-install-recommends -y
 echo "Patching application shortcuts..."
 sed -i 's/chromium-browser %U/chromium-browser --no-sandbox %U/g' /usr/share/applications/chromium-browser.desktop
-echo 'alias chromium="chromium-browser --no-sandbox" >> /etc/profile'
-echo "You can now start chromium by using the application icon or by typing chromium" && . /etc/profile
