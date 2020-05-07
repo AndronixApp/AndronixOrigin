f2a972f99d7f454adafc68ac1d49eca73ac89acd
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-05-07 16:03:03 +0530

    Update manjaro.sh

--- Installer/Manjaro/manjaro.sh
+++ Installer/Manjaro/manjaro.sh
@@ -80,7 +80,7 @@ EOL
 rm -rf $folder/etc/resolv.conf && echo "nameserver 1.1.1.1" > $folder/etc/resolv.conf
 echo "pacman-mirrors -g -c  Japan && pacman -Syyuu --noconfirm && pacman-key --init && pacman-key --populate && pacman -Syu --noconfirm" > $folder/usr/local/bin/fix-repo
 chmod +x $folder/usr/local/bin/fix-repo
-rm -rf $folder/root/.bash_profile && echo "pacman-key --init && pacman-key --populate && pacman -Syu --noconfirm" > $folder/root/.bash_profile
+rm -rf $folder/root/.bash_profile && echo "pacman-key --init && pacman-key --populate && pacman -Syu --noconfirm && rm -rf ~/.bash_profile" > $folder/root/.bash_profile
 rm -rf manjaro.partaa manjaro.partab manjaro.partac manjaro.tar.xz manjaro.sh
 
 echo "You can now launch Manjaro Linux with the ./${bin} script next time"
