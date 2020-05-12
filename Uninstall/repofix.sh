5cbd6c4d227766c144e3282480361c3c204a0ce8
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-05-12 07:17:16 +0530

    Update repofix.sh

--- Uninstall/repofix.sh
+++ Uninstall/repofix.sh
@@ -1,39 +1,18 @@
 
-
 #!/bin/bash
 folder=manjaro-fs
 folder2=androjaro-fs
 if [ -d "$folder" ]; then
 
-    rm -rf manjaro-fs/etc/pacman.d/mirrorlist
-	cat > $folder/etc/pacman.d/mirrorlist <<- EOM
-	##
-	## Manjaro Linux repository mirrorlist
-	## Generated on 02 May 2020 14:22
-	##
-	## Use pacman-mirrors to modify
-	##
-	## Location  : Germany
-	## Time      : 99.99
-	## Last Sync :
-	Server = http://manjaro-arm.moson.eu/arm-stable/$repo/$arch/
-	EOM
+    rm -rf $folder/etc/pacman.d/mirrorlist
+    wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Uninstall/mirrorlist -O $folder/etc/pacman.d/mirrorlist
+    sed -i '1s/^/pacman-key --init \&\& pacman-key --populate \&\& pacman -Syu --noconfirm\n/' $folder/root/.bash_profile
 fi
-
 
 if [ -d "$folder2" ]; then
 
-    rm -rf androjaro-fs/etc/pacman.d/mirrorlist
-	cat > $folder2/etc/pacman.d/mirrorlist <<- EOM
-	##
-	## Manjaro Linux repository mirrorlist
-	## Generated on 02 May 2020 14:22
-	##
-	## Use pacman-mirrors to modify
-	##
-	## Location  : Germany
-	## Time      : 99.99
-	## Last Sync :
-	Server = http://manjaro-arm.moson.eu/arm-stable/$repo/$arch/
-	EOM
+    rm -rf $folder2/etc/pacman.d/mirrorlist
+    wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Uninstall/mirrorlist -O $folder2/etc/pacman.d/mirrorlist
+    sed -i '1s/^/pacman-key --init \&\& pacman-key --populate \&\& pacman -Syu --noconfirm\n/' $folder2/root/.bash_profile
+
 fi
