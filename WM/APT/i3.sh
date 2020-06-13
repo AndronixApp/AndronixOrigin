11e595493fe594ac356356b84c7754850fd2fb77
ultrahacx <anony3039@gmail.com>
2020-06-13 20:58:04 +0530

    WM test

---
+++ WM/APT/i3.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+clear
+echo "Installing i3wm"
+sleep 2
+sudo apt update -y
+sudo apt install i3 tigervnc-standalone-server wget nano dbus-x11 xorg xterm xfce4-terminal pcmanfm shotwell feh cairo-dock libexo-1-0 tigervnc-common --no-install-recommends -y
+clear
+
+read -p "Want to install default browser ? (y/n)" choice
+case "$choice" in 
+  y|Y ) sudo apt install epiphany-browser -y ;;
+  n|N ) echo "Ok... Not epiphany browser";;
+  * ) echo "invalid";;
+esac
+
+read -p "What to install chromium browser ? (y/n) [ Chromium might not work on arm/arm32/armhf devices ] " choice
+case "$choice" in 
+  y|Y ) wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Uninstall/ubchromiumfix.sh && chmod +x ubchromiumfix.sh && ./ubchromiumfix.sh && rm -rf ubchromiumfix.sh ;;
+  n|N ) echo "Ok... Not installing Chromium";;
+  * ) echo "invalid";;
+esac
+
+mkdir -p ~/.vnc
+
+echo "#!/bin/bash
+[ -r ~/.Xresources ] && xrdb ~/.Xresources
+export PULSE_SERVER=127.0.0.1
+export DISPLAY=:1
+export ~/.Xauthority
+dbus-launch i3 &
+dbus-launch cairo-dock & " > ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
+
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/LXDE/vncserver-start -O /usr/local/bin/vncserver-start
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/LXDE/vncserver-stop -O /usr/local/bin/vncserver-stop
+chmod +x /usr/local/bin/vncserver-start
+chmod +x /usr/local/bin/vncserver-stop
+
+
+echo "You can now start vncserver by running vncserver-start"
+echo " "
+echo "It will ask you to enter a password when first time starting it."
+echo " "
+echo "The VNC Server will be started at 127.0.0.1:5901"
+echo " "
+echo "You can connect to this address with a VNC Viewer you prefer"
+echo ""
+echo ""
+echo ""
+echo "Running vncserver-start"
+echo ""
+echo ""
+echo ""
+echo " To Kill VNC Server just run vncserver-stop "
+echo ""
+echo ""
+echo ""
+
+vncpasswd
+vncserver-start
---
+++ WM/Pacman/openbox.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+clear
+echo "Installing Openbox"
+sleep 2
+pacman -Syu --noconfirm
+pacman -S openbox obconf tigervnc wget nano dbus xorg xterm xfce4-terminal pcmanfm shotwell feh cairo-dock --noconfirm
+clear
+
+read -p "Want to install default browser ? (y/n)" choice
+case "$choice" in 
+  y|Y ) pacman -S epiphany --noconfirm ;;
+  n|N ) echo "Ok... Not epiphany browser";;
+  * ) echo "invalid";;
+esac
+
+read -p "What to install chromium browser ? (y/n) [ Chromium might not work on arm/arm32/armhf devices ] " choice
+case "$choice" in 
+  y|Y ) pacman -S chromium --noconfirm ;;
+  n|N ) echo "Ok...Not installing Chromium";;
+  * ) echo "invalid";;
+esac
+
+mkdir -p ~/.vnc
+
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/WM/wallpaper.jpg -O /usr/share/wallpaper.jpg
+
+echo "#!/bin/bash
+[ -r ~/.Xresources ] && xrdb ~/.Xresources
+export PULSE_SERVER=127.0.0.1
+export DISPLAY=:1
+XAUTHORITY=~/.Xauthority
+export XAUTHORITY
+dbus-launch openbox &
+dbus-launch cairo-dock & 
+feh --bg-fill /usr/share/wallpaper.jpg" > ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
+
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Pacman/Manjaro/XFCE/vncserver-start -O /usr/local/bin/vncserver-start
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Pacman/Manjaro/XFCE/vncserver-stop -O /usr/local/bin/vncserver-stop
+chmod +x /usr/local/bin/vncserver-start
+chmod +x /usr/local/bin/vncserver-stop
+
+
+echo "You can now start vncserver by running vncserver-start"
+echo ""
+echo "It will ask you to enter a password when first time starting it."
+echo ""
+echo "The VNC Server will be started at 127.0.0.1:5901"
+echo ""
+echo "You can connect to this address with a VNC Viewer you prefer"
+echo ""
+echo ""
+echo ""
+echo "Running vncserver-start"
+echo ""
+echo ""
+echo ""
+echo "To Kill VNC Server just run vncserver-stop"
+echo ""
+echo ""
+echo ""
+
+vncpasswd
+vncserver-start
