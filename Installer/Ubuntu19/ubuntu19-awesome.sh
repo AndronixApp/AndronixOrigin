bb0feff6b88e74c3e1d3393f69db33b306c011bc
ultrahacx <anony3039@gmail.com>
2020-06-16 19:34:35 +0530

    WM installer test

---
+++ Installer/Ubuntu19/ubuntu19-awesome.sh
@@ -0,0 +1,128 @@
+#!/data/data/com.termux/files/usr/bin/bash
+folder=ubuntu19-fs
+dlink="https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/APT"
+dlink2="https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/WM/APT"
+if [ -d "$folder" ]; then
+	first=1
+	echo "skipping downloading"
+fi
+tarball="ubuntu19-rootfs.tar.gz"
+
+if [ "$first" != 1 ];then
+	if [ ! -f $tarball ]; then
+		echo "Download Rootfs, this may take a while base on your internet speed."
+		case `dpkg --print-architecture` in
+		aarch64)
+			archurl="arm64" ;;
+		arm)
+			archurl="armhf" ;;
+		amd64)
+			archurl="amd64" ;;
+		x86_64)
+			archurl="amd64" ;;	
+		i*86)
+			archurl="i386" ;;
+		x86)
+			archurl="i386" ;;
+		*)
+			echo "unknown architecture"; exit 1 ;;
+		esac
+		wget "https://github.com/AndronixApp/AndronixOrigin/raw/master/Rootfs/Ubuntu19/ubuntu-19.10-${archurl}.tar.gz" -O $tarball
+
+fi
+	cur=`pwd`
+	mkdir -p "$folder"
+	cd "$folder"
+	echo "Decompressing Rootfs, please be patient."
+	proot --link2symlink tar -xf ${cur}/${tarball} --exclude=dev||:
+	cd "$cur"
+fi
+mkdir -p ubuntu19-binds
+bin=start-ubuntu19.sh
+echo "writing launch script"
+cat > $bin <<- EOM
+#!/bin/bash
+cd \$(dirname \$0)
+## unset LD_PRELOAD in case termux-exec is installed
+unset LD_PRELOAD
+command="proot"
+command+=" --link2symlink"
+command+=" -0"
+command+=" -r $folder"
+if [ -n "\$(ls -A ubuntu19-binds)" ]; then
+    for f in ubuntu19-binds/* ;do
+      . \$f
+    done
+fi
+command+=" -b /dev"
+command+=" -b /proc"
+command+=" -b ubuntu19-fs/root:/dev/shm"
+## uncomment the following line to have access to the home directory of termux
+#command+=" -b /data/data/com.termux/files/home:/root"
+## uncomment the following line to mount /sdcard directly to / 
+#command+=" -b /sdcard"
+command+=" -w /root"
+command+=" /usr/bin/env -i"
+command+=" HOME=/root"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+command+=" TERM=\$TERM"
+command+=" LANG=C.UTF-8"
+command+=" /bin/bash --login"
+com="\$@"
+if [ -z "\$1" ];then
+    exec \$command
+else
+    \$command -c "\$com"
+fi
+EOM
+
+mkdir -p ubuntu19-fs/var/tmp
+rm -rf ubuntu19-fs/usr/local/bin/*
+
+wget -q https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/.profile -O ubuntu19-fs/root/.profile.1 > /dev/null
+cat $folder/root/.profile.1 >> $folder/root/.profile && rm -rf $folder/root/.profile.1
+wget -q https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vnc -P ubuntu19-fs/usr/local/bin > /dev/null
+wget -q https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vncpasswd -P ubuntu19-fs/usr/local/bin > /dev/null
+wget -q https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vncserver-stop -P ubuntu19-fs/usr/local/bin > /dev/null
+wget -q https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vncserver-start -P ubuntu19-fs/usr/local/bin > /dev/null
+
+chmod +x ubuntu19-fs/root/.bash_profile
+chmod +x ubuntu19-fs/root/.profile
+chmod +x ubuntu19-fs/usr/local/bin/vnc
+chmod +x ubuntu19-fs/usr/local/bin/vncpasswd
+chmod +x ubuntu19-fs/usr/local/bin/vncserver-start
+chmod +x ubuntu19-fs/usr/local/bin/vncserver-stop
+
+echo "fixing shebang of $bin"
+termux-fix-shebang $bin
+echo "making $bin executable"
+chmod +x $bin
+echo "removing image for some space"
+rm $tarball
+
+#DE installation addition
+
+wget --tries=20 $dlink2/awesome.sh -O $folder/awesome.sh
+clear
+echo "Setting up the installation of LXDE VNC"
+
+echo "APT::Acquire::Retries \"3\";" > $folder/etc/apt/apt.conf.d/80-retries #Setting APT retry count
+echo "#!/bin/bash
+apt update -y && apt install wget sudo -y
+clear
+if [ ! -f /root/awesome.sh ]; then
+    wget --tries=20 $dlink2/awesome.sh -O /root/awesome.sh
+    bash ~/awesome.sh
+else
+    bash ~/awesome.sh
+fi
+clear
+
+if [ ! -f /usr/bin/vncserver ]; then
+    apt install tigervnc-standalone-server -y
+fi
+clear
+echo ' Welcome to Andronix Ubuntu 19 | Awesome '
+rm -rf ~/.bash_profile" > $folder/root/.bash_profile 
+
+bash $bin
---
+++ Installer/Void/void-i3.sh
@@ -0,0 +1,121 @@
+#!/data/data/com.termux/files/usr/bin/bash
+folder=void-fs
+dlink="https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/XBPS"
+dlink2="https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/WM/XBPS"
+echo "Please enable Termux storage permission if it asks for so"
+sleep 3
+termux-setup-storage
+
+if [ -d "$folder" ]; then
+  first=1
+  echo "skipping downloading"
+fi
+tarball="void.tar.xz"
+
+
+
+if [ "$first" != 1 ];then
+  if [ ! -f $tarball ]; then
+    echo "Download Rootfs, this may take a while base on your internet speed."
+    case `dpkg --print-architecture` in
+    aarch64)
+      archurl="arm64" ;;
+    arm)
+      archurl="armhf" ;;
+    amd64)
+      archurl="amd64" ;;
+    x86_64)
+      archurl="amd64" ;;  
+    i*86)
+      archurl="i386" ;;
+    x86)
+      archurl="i386" ;;
+    *)
+      echo "unknown architecture"; exit 1 ;;
+    esac
+    wget "https://github.com/AndronixApp/AndronixOrigin/blob/master/Rootfs/Void/${archurl}/void_${archurl}.tar.xz?raw=true" -O $tarball
+  fi
+  mkdir -p "$folder"
+  echo "Decompressing Rootfs, please be patient."
+  proot --link2symlink tar -xJf ${tarball} -C $folder||:
+fi
+
+mkdir -p void-binds
+bin=start-void.sh
+echo "writing launch script"
+cat > $bin <<- EOM
+#!/bin/bash
+cd \$(dirname \$0)
+## unset LD_PRELOAD in case termux-exec is installed
+unset LD_PRELOAD
+command="proot"
+command+=" --link2symlink"
+command+=" -0"
+command+=" -r $folder"
+if [ -n "\$(ls -A void-binds)" ]; then
+    for f in void-binds/* ;do
+      . \$f
+    done
+fi
+command+=" -b /dev"
+command+=" -b /proc"
+command+=" -b void-fs/root:/dev/shm"
+## uncomment the following line to have access to the home directory of termux
+#command+=" -b /data/data/com.termux/files/home:/root"
+## uncomment the following line to mount /sdcard directly to / 
+command+=" -b /sdcard"
+command+=" -w /root"
+command+=" /usr/bin/env -i"
+command+=" HOME=/root"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+command+=" TERM=\$TERM"
+command+=" LANG=C.UTF-8"
+command+=" /bin/bash --login"
+com="\$@"
+if [ -z "\$1" ];then
+    exec \$command
+else
+    \$command -c "\$com"
+fi
+EOM
+
+echo "fixing shebang of $bin"
+termux-fix-shebang $bin
+
+
+
+echo "Fixing DNS for internet connection"
+rm -rf void-fs/etc/resolv.conf
+echo "nameserver 8.8.8.8
+nameserver 8.8.4.4
+nameserver 192.168.1.1
+nameserver 127.0.0.1" > void-fs/etc/resolv.conf
+
+echo "making $bin executable"
+chmod +x $bin
+rm $tarball
+
+#DE installation addition
+
+wget --tries=20 $dlink2/i3.sh -O $folder/root/i3.sh > /dev/null
+clear
+echo "Setting up the installation of Openbox VNC"
+
+echo "#!/bin/bash
+clear
+ echo 'Installing i3... This might take some time depending upon your Internet'
+xbps-install -Su -y > /dev/null 
+xbps-install -S lxde tigervnc wget sudo -y 
+clear
+if [ ! -f /root/i3.sh ]; then
+    wget --tries=20 $dlink2/i3.sh -O /root/i3.sh > /dev/null
+    bash ~/i3.sh 
+else
+    bash ~/i3.sh
+fi
+clear
+if [ ! -f /usr/bin/vncserver ]; then
+    xbps-install -S i3 tigervnc wget -y  > /dev/null
+fi
+rm -rf ~/.bash_profile" > $folder/root/.bash_profile 
+bash $bin
