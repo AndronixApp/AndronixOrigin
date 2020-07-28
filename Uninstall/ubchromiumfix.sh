4ef7365294e5165b285b03fee57ab4be64609869
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-07-29 04:20:42 +0530

    Added new method of installing Chromium 🔥

--- Uninstall/ubchromiumfix.sh
+++ Uninstall/ubchromiumfix.sh
@@ -3,7 +3,7 @@
 echo "Removing distribution provided chromium packages and dependencies..."
 apt purge chromium* chromium-browser* snapd -y -qq && apt autoremove -y -qq
 sudo apt purge chromium* chromium-browser* -y -qq && apt autoremove -y -qq
-
+apt update -qq; apt install software-properties-common gnupg --no-install-recommends -y -qq
 echo "Adding Debian repo for Chromium installation"
 
 cat <<EOT >> /etc/apt/preferences.d/chromium.pref
