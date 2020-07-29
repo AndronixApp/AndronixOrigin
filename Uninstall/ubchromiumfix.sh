7fe5ec3becd02f26723f2cc463af10e8271279ac
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-07-29 16:19:06 +0530

    Fixed Chromium menu entry ✌️

--- Uninstall/ubchromiumfix.sh
+++ Uninstall/ubchromiumfix.sh
@@ -17,4 +17,4 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A
 apt update -y
 apt install chromium -y
 
-sed -i 's/chromium-browser %U/chromium --no-sandbox %U/g' /usr/share/application/chromium-browser.desktop
+sed -i 's/chromium %U/chromium --no-sandbox %U/g' /usr/share/applications/chromium.desktop
