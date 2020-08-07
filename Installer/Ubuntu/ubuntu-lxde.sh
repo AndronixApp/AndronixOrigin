cd486ee992ca8adbad912081104f39a8bcc1ab20
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-08-07 12:57:00 +0530

    Added wget for scripts✔

--- Installer/Ubuntu/ubuntu-lxde.sh
+++ Installer/Ubuntu/ubuntu-lxde.sh
@@ -1,4 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
+pkg install wget -y 
 folder=ubuntu-fs
 dlink="https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/APT"
 if [ -d "$folder" ]; then
