b7db62ae65013a67b036767cb5a7de546b610af5
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-08-11 19:11:48 +0530

    Update lxqt19.sh

--- APT/LXQT/lxqt19.sh
+++ APT/LXQT/lxqt19.sh
@@ -3,8 +3,9 @@
 #Get the necessary components
 sudo apt-mark hold udisks2
 sudo apt-get update -y
-apt-get install keyboard-configuration -y
-apt-get install sudo wget -y
+sudo apt-get install keyboard-configuration -y
+sudo apt-get install tzdata -y
+sudo apt-get install sudo wget nano inetutils-tools -y
 sudo apt-get install lxqt-core lxqt-config qterminal tigervnc-standalone-server dbus-x11 openbox -y
 sudo apt-get install xfe -y
 sudo apt-get clean
