4bba1bc719eac427810e48a398309e79ff74f7f2
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-08-11 21:43:10 +0530

    Update lxde19.sh

--- APT/LXDE/lxde19.sh
+++ APT/LXDE/lxde19.sh
@@ -5,7 +5,7 @@ sudo apt-mark hold udisks2
 sudo apt-get update
 sudo apt-get install keyboard-configuration -y
 sudo apt-get install tzdata -y
-sudo apt-get install sudo wget nano inetutils-tools -y
+sudo apt-get install sudo wget nano inetutils-tools dialog -y
 sudo apt-get install lxde-core lxterminal tigervnc-standalone-server tigervnc-common dbus-x11 --no-install-recommends -y
 sudo apt-get clean
 
