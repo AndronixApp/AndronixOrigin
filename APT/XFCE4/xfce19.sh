43826d3c004b4f4e27c65cdd907f6a956fcae9d2
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-08-11 21:43:37 +0530

    Update xfce19.sh

--- APT/XFCE4/xfce19.sh
+++ APT/XFCE4/xfce19.sh
@@ -8,7 +8,7 @@ sudo dpkg --configure -a
 sudo apt-mark hold udisks2
 sudo apt-get install keyboard-configuration -y
 sudo apt-get install tzdata -y
-sudo apt-get install sudo wget nano inetutils-tools -y
+sudo apt-get install sudo wget nano inetutils-tools dialog -y
 sudo apt-get install xfce4 xfce4-goodies xfce4-terminal exo-utils tigervnc-standalone-server tigervnc-common dbus-x11 --no-install-recommends -y
 sudo apt-get clean
 mkdir -p ~/.vnc
