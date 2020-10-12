3043178225aa16e00c7360e782c3e7e80d4836a9
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2020-10-12 22:02:14 +0530

    Fix for tigervnc

--- Pacman/tigervnc-fix.sh
+++ Pacman/tigervnc-fix.sh
@@ -1,4 +1,4 @@
 pacman -S tar wget --noconfirm
-pacman -U https://raw.githubusercontent.com/ultrahacx/AndronixOrigin/master/tigervnc-1.10.1-1-aarch64.pkg.tar.xz --noconfirm
-curl -s https://raw.githubusercontent.com/ultrahacx/AndronixOrigin/master/lib.tar.xz -o /usr/lib/a.tar.xz && tar xf /usr/lib/a.tar.xz -C /usr/lib
+pacman -U https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/tigervnc-1.10.1-1-aarch64.pkg.tar.xz --noconfirm
+curl -s https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/lib.tar.xz -o /usr/lib/a.tar.xz && tar xf /usr/lib/a.tar.xz -C /usr/lib
 sed -i '27i IgnorePkg = tigervnc' /etc/pacman.conf
