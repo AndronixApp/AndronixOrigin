b98b751468afa7b6379354e4251c8e37f6c84719
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2021-06-26 12:04:20 +0530

    Updated Manjaro ARM repo

--- Installer/Manjaro/manjaro-openbox.sh
+++ Installer/Manjaro/manjaro-openbox.sh
@@ -83,7 +83,7 @@ cat >$folder/etc/pacman.d/mirrorlist <<'EOL'
 ## Location  : Germany
 ## Time      : 99.99
 ## Last Sync :
-Server = http://manjaro-arm.moson.eu/arm-stable/$repo/$arch/
+Server = https://mirrors.dotsrc.org/manjaro-arm/stable/$repo/$arch
 EOL
 rm -rf $folder/etc/resolv.conf && echo "nameserver 1.1.1.1" > $folder/etc/resolv.conf
 wget --tries=20  $dlink/XFCE/vncserver-start -O $folder/usr/local/bin/vncserver-start 
