7c691f42ff1b26b56f43bd524ca79a7f3f190d69
Anurag Verma <43913907+ultrahacx@users.noreply.github.com>
2021-08-08 19:47:18 +0530

    Update VSCode to Official VSCode ARM

--- Uninstall/vscode_patch.sh
+++ Uninstall/vscode_patch.sh
@@ -1,91 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Detecting architecture...";
-MACHINE_MTYPE="$(uname -m)";
-ARCH="${MACHINE_MTYPE}";
-REPO_VENDOR="headmelted";
+url="https://github.com/AndronixApp/andronix-external-app/releases/download/1.0.0/code-stable-"
+ARCH=""
 
-echo "Ensuring curl is installed";
-apt-get install -y gnupg curl;
+red="\033[0;31m"
+yellow="\033[1;33m"
+green="\033[0;32m"
+ncolor="\033[0m"
 
-if [ "$ARCH" = "amd64" ] || [ "$ARCH" = "i386" ]; then REPO_VENDOR="microsoft"; fi;
+echo "${green}Verifying architecture..."
+case `dpkg --print-architecture` in
+aarch64)
+        ARCH="arm64" ;;
+arm64)
+        ARCH="arm64" ;;
+arm)
+        ARCH="armhf" ;;
+*)
+        echo -e "${red}Architecture not supported! Exiting...${ncolor}"; exit 1 ;;
+esac
 
-echo "Architecture detected as $ARCH...";
+echo -e "${green}Detected architecture: $ARCH${ncolor}"
 
-if [ "${REPO_VENDOR}" = "headmelted" ]; then
-  gpg_key=https://packagecloud.io/headmelted/codebuilds/gpgkey;
-  repo_name="stretch";
-  repo_entry="deb https://packagecloud.io/headmelted/codebuilds/debian/ ${repo_name} main";
-  code_executable_name="code-oss";
+echo "Verifying dependencies..."
+apt update -y
+apt install sudo -y
+sudo apt install wget tar -y
+
+
+if [ -d "/opt" ]
+then
+    echo -e "${green}/opt exists. Installing VSCode in /opt${ncolor}"
+    wget $url${ARCH}.tar.gz -O /opt/vscode.tar.gz
 else
-  gpg_key=https://packages.microsoft.com/keys/microsoft.asc;
-  repo_name="stable"
-  repo_entry="deb https://packages.microsoft.com/repos/vscode ${repo_name} main";
-  code_executable_name="code-insiders";
-fi;
+    echo -e "${yellow}/opt not found. Creating /opt directory and installing VSCode in /opt${ncolor}" 
+    mkdir /opt
+    wget $url${ARCH}.tar.gz -O /opt/vscode.tar.gz
+fi
 
-echo "Retrieving GPG key [${REPO_VENDOR}] ($gpg_key)...";
-# import the gpg key
-curl -L "${gpg_key}" 2> /dev/null | apt-key add - &>/dev/null
-echo "done."
+mkdir /opt/vscode
+tar xf /opt/vscode.tar.gz -C /opt/vscode --strip-components=1
+rm -rf /opt/vscode.tar.gz
 
-echo "Removing any previous entry to headmelted repository";
-rm -rf /etc/apt/sources.list.d/headmelted_codebuilds.list;
-rm -rf /etc/apt/sources.list.d/codebuilds.list;
-  
-echo "Installing [${REPO_VENDOR}] repository...";
-echo "${repo_entry}" > /etc/apt/sources.list.d/${REPO_VENDOR}_vscode.list;
-  
-echo "Updating APT cache..."
-apt-get update -yq;
-echo "Done!"
+echo -e "alias code='/opt/vscode/code --no-sandbox'" >> ~/.bashrc
 
-if [ $? -eq 0 ]; then
-  echo "Repository install complete.";
-else
-  echo "Repository install failed.";
-  exit 1;
-fi;
-
-echo "Installing Visual Studio Code from [${repo_name}]...";
-apt-get install -t ${repo_name} -y ${code_executable_name};
-#apt-get install -t ${repo_name} -y --allow-unauthenticated ${code_executable_name};
-
-if [ $? -eq 0 ]; then
-  echo "Visual Studio Code install complete.";
-else
-  echo "Visual Studio Code install failed.";
-  exit 1;
-fi;
-
-echo "Installing git...";
-apt-get install -y git;
-
-if [ $? -eq 0 ]; then
-  echo "git install complete.";
-else
-  echo "git install failed.";
-  exit 1;
-fi;
-
-echo "Installing any dependencies that may have been missed...";
-apt-get install -y -f;
-
-if [ $? -eq 0 ]; then
-  echo "Missed dependency install complete.";
-else
-  echo "Missed dependency install failed.";
-  exit 1;
-fi;
-
-echo "
-
-Installation complete!
-
-You can start code at any time by calling \"${code_executable_name}\" within a terminal.
-
-A shortcut should also now be available in your desktop menus (depending on your distribution).
-
-";
-sed -i 's/code-oss /code-oss --user-data-dir=\/root /g' /usr/share/applications/code-oss*
-echo "alias code-oss='code-oss --user-data-dir=/root'" >> /etc/profile
+echo -e "${green}Installation complete! Run command ${red}code${ncolor} to launch VSCode"
