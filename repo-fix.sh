22f703930f88cc1d71d69e55bc0a70dc3c759264
Prakhar Shukla <imprakharshukla@gmail.com>
2022-08-29 15:08:35 +0530

    Update repo-fix.sh

--- repo-fix.sh
+++ repo-fix.sh
@@ -1,1 +1,1 @@
-echo "deb https://grimler.se/termux-packages-24 stable main" > $PREFIX/etc/apt/sources.list
+echo "deb https://termux.mentality.rip/termux-main stable main" > $PREFIX/etc/apt/sources.list
